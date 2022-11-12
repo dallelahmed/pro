029d14d145265015c70803a282f28343166466ad
Ahmed Dallel <ahmeddallel9@gmail.com>
2022-11-12 21:25:49 +0100

    Implicit

--- app/src/main/java/com/example/implicitintents/MainActivity.java
+++ app/src/main/java/com/example/implicitintents/MainActivity.java
@@ -1,35 +1,34 @@
 package com.example.implicitintents;
 
-import static android.content.Intent.ACTION_CAMERA_BUTTON;
-
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.provider.MediaStore;
 
 public class MainActivity extends AppCompatActivity {
     public static final int CAMERA_PERMISSION_REQUEST_CODE = 2132315;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
     Button camera;
     private EditText mWebsiteEditText;
     private EditText mLocationEditText;
     private EditText mShareTextEditText;
-    static final int REQUEST_IMAGE_CAPTURE=1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        }
+    }
 
     public void openWebsite(View view) {
         String url = mWebsiteEditText.getText().toString();
@@ -42,16 +41,13 @@ public class MainActivity extends AppCompatActivity {
             Log.d("ImplicitIntents", "Can't handle this!");
         }
     }
+
     public void openLocation(View view) {
-        // Get the string indicating a location. Input is not validated; it is
-        // passed to the location handler intact.
+
         String loc = mLocationEditText.getText().toString();
 
-        // Parse the location and create the intent.
         Uri addressUri = Uri.parse("geo:0,0?q=" + loc);
         Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
-
-        // Find an activity to handle the intent, and start that activity.
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         } else {
@@ -63,49 +59,35 @@ public class MainActivity extends AppCompatActivity {
         mShareTextEditText = findViewById(R.id.share_edittext);
         String txt = mShareTextEditText.getText().toString();
         String mimeType = "text/plain";
-        ShareCompat.IntentBuilder
-                .from(this)
-                .setType(mimeType)
-                .setChooserTitle("Share this text with: ")
-                .setText(txt)
-                .startChooser();
-
+        ShareCompat.IntentBuilder.from(this).setType(mimeType).setChooserTitle("Share this text with: ").setText(txt).startChooser();
     }
-    public void onTakePhotoClick(View v){
+
+    public void onTakePhotoClick(View v) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
                 takePicture();
-            }
-            else{
+            } else {
                 String[] permissionRequest = {Manifest.permission.CAMERA};
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                     requestPermissions(permissionRequest, CAMERA_PERMISSION_REQUEST_CODE);
                 }
             }
         }
-
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE){
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 takePicture();
             }
         }
     }
 
     public void takePicture() {
-        Intent intent= new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivity(intent);
-
-
-
-       /* if (intent.resolveActivity(getPackageManager()) != null){
-            startActivityForResult(intent,REQUEST_IMAGE_CAPTURE);*/
-
-        }
-
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivity(intent);
 
     }
+}
