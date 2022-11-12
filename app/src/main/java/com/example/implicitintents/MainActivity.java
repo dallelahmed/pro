8feb75a71a14a5e43a7b6c16af9f0af2d8f1586a
Ahmed Dallel <ahmeddallel9@gmail.com>
2022-11-12 21:16:34 +0100

    Implicit

--- app/src/main/java/com/example/implicitintents/MainActivity.java
+++ app/src/main/java/com/example/implicitintents/MainActivity.java
@@ -1,25 +1,33 @@
 package com.example.implicitintents;
 
+import static android.content.Intent.ACTION_CAMERA_BUTTON;
+
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.provider.MediaStore;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final int CAMERA_PERMISSION_REQUEST_CODE = 2132315;
+    Button camera;
     private EditText mWebsiteEditText;
     private EditText mLocationEditText;
     private EditText mShareTextEditText;
+    static final int REQUEST_IMAGE_CAPTURE=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mWebsiteEditText = findViewById(R.id.website_edittext);
-
 
         }
 
@@ -27,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         String url = mWebsiteEditText.getText().toString();
         Uri webpage = Uri.parse(url);
         Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         } else {
@@ -49,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
             Log.d("ImplicitIntents", "Can't handle this intent!");
         }
     }
+
     public void shareText(View view) {
         mShareTextEditText = findViewById(R.id.share_edittext);
         String txt = mShareTextEditText.getText().toString();
@@ -61,5 +71,41 @@ public class MainActivity extends AppCompatActivity {
                 .startChooser();
 
     }
+    public void onTakePhotoClick(View v){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                takePicture();
+            }
+            else{
+                String[] permissionRequest = {Manifest.permission.CAMERA};
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    requestPermissions(permissionRequest, CAMERA_PERMISSION_REQUEST_CODE);
+                }
+            }
+        }
 
-}
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                takePicture();
+            }
+        }
+    }
+
+    public void takePicture() {
+        Intent intent= new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivity(intent);
+
+
+
+       /* if (intent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(intent,REQUEST_IMAGE_CAPTURE);*/
+
+        }
+
+
+    }
